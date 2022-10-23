55669497d2c476757f6af1d466826813a113db0f
t1babin <62140444+t1babin@users.noreply.github.com>
2022-10-23 13:35:41 -0400

    Add files via upload

---
+++ compileOS.sh
@@ -0,0 +1,5 @@
+
+bcc -ansi -c -o kernel_c.o kernel.c
+as86 kernel.asm -o kernel_asm.o
+ld86 -o kernel -d kernel_c.o kernel_asm.o
+dd if=kernel of=diskc.img bs=512 conv=notrunc seek=3
---
+++ kernel.c
@@ -0,0 +1,22 @@
+
+//Isaiah's Operating System Kernel
+//kernel.c
+
+void main(){
+
+	int startVidMem = 0xb800;
+	int vidMemOffset = 0x0;
+	int white = 0x7;
+	char* letters = "Hello World\0";
+
+	while(*letters != 0x0) {
+		putInMemory(startVidMem, vidMemOffset, *letters);
+		vidMemOffset++;
+		putInMemory(startVidMem, vidMemOffset, white);
+		vidMemOffset++;
+		//advance letters pointer
+		letters++;
+	}
+
+	while(1);
+}
