b5562d206c9b258e952cf8601be13b236b2ade7f
Timothy O'Leary <60230591+TimothyOLeary@users.noreply.github.com>
2022-10-25 16:56:18 -0400

    Add files via upload

--- kernel.c
+++ kernel.c
@@ -1,28 +1,57 @@
-
 //Isaiah's Operating System Kernel
 //Modified by Tom and Tim
 //kernel.c
 
+void handleInterrupt21(int,int,int,int); 
+void printChar(char);
 void printstring(char*);
+void readstring(char*);
 
 void main(){
-        printstring("Hello World");
+	char line[80];
+	printstring("Enter a line: ");
+	readstring(line);
+	printstring(line);
         while(1);
+}
+
+void printChar(char c){
+	interrupt(0x10,0xe*256+c,0,0,0);
 }
 
 void printstring(char* letters){
 
-//	int startVidMem = 0xb800;
-//	int vidMemOffset = 0x0;
-//	int white = 0;
-
 	while(*letters != 0x0) {
-		//putInMemory(startVidMem, vidMemOffset, *letters);
-		//vidMemOffset++;
-		//putInMemory(startVidMem, vidMemOffset, white);
-		//vidMemOffset++;
 		interrupt(0x10,0xe*256+*letters,0,0,0);
 		//advance letters pointer
 		letters++;
 	}
 }
+
+void readstring(char output[]){
+	int i=0;
+	output[i] = interrupt(0x16,0,0,0,0);
+	interrupt(0x10,0xe*256+output[i],0,0,0);
+	while(output[i] != 0xd){
+		i++;
+		output[i] = interrupt(0x16,0,0,0,0);
+		if(output[i] == 0xa){
+			i--;
+			output[i] = 32;
+			interrupt(0x10,0xe*256+output[i],0,0,0);
+		}
+		else
+			interrupt(0x10,0xe*256+output[i],0,0,0);
+	}
+	i++;
+	output[i] = 0xa;
+	interrupt(0x10,0xe*256+output[i],0,0,0);
+	i++;
+	output[i] = 0x0;
+	interrupt(0x10,0xe*256+output[i],0,0,0);
+}
+
+void handleInterrupt21(int AX, int BX, int CX, int DX){
+	printChar('T');
+}
+
