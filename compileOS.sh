660473fbcc83efa7f6a3510e0e74e17b26b518b5
t1babin <62140444+t1babin@users.noreply.github.com>
2022-10-26 14:07:07 -0400

    Add files via upload

--- compileOS.sh
+++ compileOS.sh
@@ -1,5 +1,9 @@
+dd if=/dev/zero of=diskc.img bs=512 count=256
+nasm bootload.asm
+dd if=bootload of=diskc.img conv=notrunc
 
 bcc -ansi -c -o kernel_c.o kernel.c
 as86 kernel.asm -o kernel_asm.o
 ld86 -o kernel -d kernel_c.o kernel_asm.o
 dd if=kernel of=diskc.img bs=512 conv=notrunc seek=3
+dd if=message.txt of=diskc.img conv=notrunc seek=30
--- kernel.c
+++ kernel.c
@@ -6,12 +6,26 @@ void handleInterrupt21(int,int,int,int);
 void printChar(char);
 void printstring(char*);
 void readstring(char*);
+void readsector(char*, int);
+void makeInterrupt21(int,int,int,int);
 
 void main(){
 	char line[80];
+	char line2[80];
+	char buffer[512];
+	char* interruptstring = "the interrupt for printstring";
+	char buffer2[512];
+	printstring("Hello world\n");
 	printstring("Enter a line: ");
 	readstring(line);
 	printstring(line);
+	readsector(buffer, 30);
+	printstring(buffer);
+	makeInterrupt21();
+	interrupt(0x21,0,interruptstring,0,0);
+	interrupt(0x21,1,line2,0,0);
+	interrupt(0x21,2,buffer,30,0);
+	interrupt(0x21,5,0,0,0);
         while(1);
 }
 
@@ -26,6 +40,10 @@ void printstring(char* letters){
 		//advance letters pointer
 		letters++;
 	}
+}
+
+void readsector(char* buffer, int sector){
+	interrupt(0x13, 2*256+1, buffer, 0*256+(sector+1),0*256+0x80);
 }
 
 void readstring(char output[]){
@@ -56,5 +74,16 @@ void readstring(char output[]){
 }
 
 void handleInterrupt21(int AX, int BX, int CX, int DX){
-	printChar('T');
+	if (AX==0){
+	printstring(BX);
+	}
+	else if (AX==1){
+	readstring(BX);
+	}
+	else if (AX==2){
+	readsector(BX,CX);
+	}
+	else{
+	printstring("Error with interrupt 21");
+	}
 }
